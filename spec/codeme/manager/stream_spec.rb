6ed175dc25949720ab630c7bf9ff720f5ef30257
蒼時弦也 <elct9620@frost.tw>
2016-12-21 11:06:15 +0800

    Add stream rspec test

--- spec/codeme/manager/stream_spec.rb
+++ spec/codeme/manager/stream_spec.rb
@@ -1,8 +1,34 @@
 require 'spec_helper'
 
+require 'codeme/manager/stream_event_loop'
 require 'codeme/manager/stream'
 require 'codeme/manager/client'
 
 RSpec.describe Codeme::Manager::Stream do
+
+  let(:event_loop) { double(Codeme::Manager::StreamEventLoop) }
+  let(:client) { double(Codeme::Manager::Client) }
+  let(:tcp_socket) { double(TCPSocket) }
+
+  subject { described_class.new(event_loop, tcp_socket, client) }
+
+  before do
+    allow(event_loop).to receive(:attach)
+  end
+
+  describe "#receive" do
+    it do
+
+      expect(client).to receive(:parse).with(anything())
+      subject.receive([])
+    end
+  end
+
+  describe "#close" do
+    it do
+      expect(client).to receive(:close)
+      subject.close
+    end
+  end
 
 end
