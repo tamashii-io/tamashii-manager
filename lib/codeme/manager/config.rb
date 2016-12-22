a0d79114c40e95afe8f729e0c6474bd968226c48
蒼時弦也 <elct9620@frost.tw>
2016-12-22 17:48:32 +0800

    Remove custom environment config

--- lib/codeme/manager/config.rb
+++ lib/codeme/manager/config.rb
@@ -4,27 +4,6 @@ module Codeme
   module Manager
     class Config < Codeme::Config
       AUTH_TYPES = [:none, :token]
-      APPLICATION_MODE = [:test, :development, :production]
-
-      Env = Struct.new(:env) do
-        def method_missing(name, *args, &block)
-          name = name.to_s
-          return unless APPLICATION_MODE.include?(name[0..-2].to_sym) && name[-1] == "?"
-          (self[:env] || ENV['RACK_ENV'] || "development") == name[0..-2]
-        end
-
-        def ==(other)
-          other.to_s == to_s
-        end
-
-        def inspect
-          (self[:env] || ENV['RACK_ENV'] || "development")
-        end
-
-        def to_s
-          (self[:env] || ENV['RACK_ENV'] || "development")
-        end
-      end
 
       register :auth_type, :none
       register :log_file, STDOUT
@@ -39,7 +18,6 @@ module Codeme
         return Manager.logger.level if level.nil?
         Manager.logger.level = level
       end
-
     end
   end
 end
